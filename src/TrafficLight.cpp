b7ea86b1e840ca9208b789847e7237689cea1803
yugoo90 <uosuji54@gmail.com>
2023-02-02 13:25:04 -0700

    applied all required changes and also nesessary suggeestions

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -29,7 +29,8 @@ void MessageQueue<T>::send(T &&msg)
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
     std::lock_guard<std::mutex> ulock(_mutex);
-    _deque.push_back(std::move(msg));
+    _deque.clear();
+    _deque.emplace_back(std::move(msg));
     _cond.notify_one();
 }
 
@@ -83,6 +84,7 @@ void TrafficLight::cycleThroughPhases()
         // calculate time elapsed for time 1
         auto end = std::chrono::steady_clock::now();  // get end time
         int time_Elapsed = int (std::chrono::duration_cast <std::chrono::milliseconds> (end - start).count());
+        cycleDuration = cycleDuration * 1000;
 
         if(time_Elapsed > cycleDuration){
              std::lock_guard<std::mutex> lck(_mutex);
@@ -98,10 +100,12 @@ void TrafficLight::cycleThroughPhases()
             // send update message
             _queue.send(std::move(_currentPhase));
 
+            // reset clock
+            start = end;
         }
         // simulate work
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        start = end;
+       
     }
 
 }
